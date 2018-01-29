867932f920ed91977bb112a94af7e148f98856d8
John <jd0824um@go.minneapolis.edu>
2018-01-28 19:16:49 -0600

    Added a method to search for a holder by name

--- db.py
+++ db.py
@@ -26,3 +26,45 @@ def setup():
     # Closes connections
     finally:
         conn.close()
+
+
+def add_holder():
+    try:
+        # Connects to DB
+        conn = sqlite3.connect(DB_NAME)
+        cur = conn.cursor()
+
+        # Sql template/statement to add a new record holder to the database using prepared and parametrize statements
+        sql_template = 'INSERT INTO {} ({}, {}, {}) VALUES (?, ?, ?))'\
+            .format(RECORDS_TABLE, NAME, COUNTRY, NUMBER_OF_CATCHES)
+
+        sql_values = (holder.name, holder.country, holder.catches)
+
+        cur.execute(sql_template, sql_values)
+
+    except sqlite3.Error as sqle:
+        print('An error has occurred')
+        print(sqle)
+
+        # Closes connections
+    finally:
+        conn.close()
+
+
+def search_holder(name):
+    try:
+        # Connects to DB
+        conn = sqlite3.connect(DB_NAME)
+
+        # Query to search for the requested holder by name
+        sql_query = 'SELECT * FROM {} WHERE {} = {}'.format(RECORDS_TABLE, NAME, name)
+
+        return sql_query
+
+    except sqlite3.Error as sqle:
+        print('An error has occurred')
+        print(sqle)
+
+        # Closes connections
+    finally:
+        conn.close()
