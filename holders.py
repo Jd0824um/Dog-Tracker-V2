f96a71759ac8ba07a66655a5629ae83f36762728
John <jd0824um@go.minneapolis.edu>
2018-01-28 20:05:33 -0600

    added a --str method and comments

--- holders.py
+++ holders.py
@@ -1,7 +1,12 @@
 class Holders:
 
+    # Constructor for creating a record holder
     def __init__(self, name, country, catches):
         self.name = name
         self.country = country
         self.catches = catches
 
+    # To string method to formats and prints out the holders information
+    def __str__(self):
+        template = 'Name: {} Country: {} Number of Catches: {}'
+        return template.format(self.name, self.country, self.catches)
